7caf365fa0cbf4be542dedb5ea312c55600771c4
SametKiybik <samettemas05@gmail.com>
2023-06-20 23:04:27 +0300

    note

--- src/Component/TodoAppim.jsx
+++ src/Component/TodoAppim.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function TodoAppim() {
   return (
-    <div className='note'>To-Do List</div>
+    <div className='note'>To-Do List
+      <div className='noteContent'>
+        <img src='images/notepad.png' className='noteImg'/>
+
+      </div>
+
+    </div>
   )
 }
 
