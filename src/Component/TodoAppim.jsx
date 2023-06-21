e3bacceb47b0481d761446428cd0d04ea2580831
SametKiybik <samettemas05@gmail.com>
2023-06-21 20:18:14 +0300

    click2

--- src/Component/TodoAppim.jsx
+++ src/Component/TodoAppim.jsx
@@ -15,18 +15,23 @@ function TodoAppim() {
   }
 
   const listen = (e) => {
-    setInputData(e.target.value)
-  }
+    setInputData(e.target.value);
+  };
 
   const add = () => {
-    setTodos([...todos, inputData])
-    localStorage.setItem("isler", JSON.stringify([...todos, inputData]))
-  }
+    const updatedTodos = [...todos, inputData];
+    setTodos(updatedTodos);
+    localStorage.setItem("isler", JSON.stringify(updatedTodos));
+  };
+  
 
   useEffect(() => {
     let gelenTodos = JSON.parse(localStorage.getItem("isler"))
-    setTodos(gelenTodos)
+    if (gelenTodos) {
+      setTodos(gelenTodos)
+    }
   }, [])
+  
 
 
 
@@ -38,6 +43,7 @@ function TodoAppim() {
             placeholder="Please add a task"
             aria-label="Recipient's username"
             aria-describedby="basic-addon2"
+            onChange={listen}
           />
           <Button variant="outline-secondary" id="button-addon2" onClick={add}>
             Add
