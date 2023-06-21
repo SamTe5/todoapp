dcc42eddf5d3f373a2dcbca8e7009cc988d04270
SametKiybik <samettemas05@gmail.com>
2023-06-21 20:39:29 +0300

    transition

--- src/Component/TodoAppim.jsx
+++ src/Component/TodoAppim.jsx
@@ -1,86 +1,77 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/esm/Col';
-import {  Row } from 'react-bootstrap';
-
+import { Row } from 'react-bootstrap';
 
 function TodoAppim() {
-  const [inputData, setInputData] = useState("")
-  const [inputVisible, setInputVisible] = useState(false)
-  const [todos, setTodos] = useState([])
+  const [inputData, setInputData] = useState('');
+  const [inputVisible, setInputVisible] = useState(false);
+  const [todos, setTodos] = useState([]);
 
+  const toggleInputVisibility = () => {
+    setInputVisible(!inputVisible);
+  };
 
-
-  const resimTikla = () => {
-    setInputVisible(!inputVisible)
-  }
-
-  const listen = (e) => {
+  const handleInputChange = (e) => {
     setInputData(e.target.value);
   };
 
-  const add = () => {
+  const addTodo = () => {
     const updatedTodos = [...todos, inputData];
     setTodos(updatedTodos);
-    localStorage.setItem("isler", JSON.stringify(updatedTodos));
+    localStorage.setItem('isler', JSON.stringify(updatedTodos));
   };
 
-
   useEffect(() => {
-    let gelenTodos = JSON.parse(localStorage.getItem("isler"))
+    const gelenTodos = JSON.parse(localStorage.getItem('isler'));
     if (gelenTodos) {
-      setTodos(gelenTodos)
+      setTodos(gelenTodos);
     }
-  }, [])
-
-
-
+  }, []);
 
   return (
-    <div className='note'>
+    <div className="note">
       <h3>To-Do List</h3>
-      <div className='noteContent'>
-        <InputGroup className="my-3 px-5 " style={{ visibility: inputVisible ? 'visible' : 'hidden' }} onKeyUp={listen}>
+      <div className="noteContent">
+        <InputGroup
+          className={`my-3 px-5 ${inputVisible ? 'input-visible' : 'input-hidden'}`}
+          onKeyUp={handleInputChange}
+        >
           <Form.Control
             placeholder="Please add a task"
             aria-label="Recipient's username"
             aria-describedby="basic-addon2"
-            onChange={listen}
+            onChange={handleInputChange}
           />
-          <Button variant="outline-secondary" id="button-addon2" onClick={add}>
+          <Button variant="outline-secondary" id="button-addon2" onClick={addTodo}>
             Add
           </Button>
-          
-
         </InputGroup>
-        <img src='images/notepad.png' className='noteImg' onClick={resimTikla} />
+        <img src="images/notepad.png" className="noteImg" onClick={toggleInputVisibility} />
         <Row>
           <Col>
-          {
-            todos.map((element, index) => {
-              return <div className='todos' key={index}>
+            {todos.map((element, index) => (
+              <div className="todos" key={index}>
                 <Row>
                   <Col>
-                  <div className='todo'>
-                    <h5>{element}</h5>
-                  </div>
+                    <div className="todo">
+                      <ul>
+                        <li>
+                          <h4>{element}</h4>
+                        </li>
+                      </ul>
+                    </div>
                   </Col>
-                  
                 </Row>
-
               </div>
-            })
-          }
+            ))}
           </Col>
         </Row>
-
-
       </div>
-
     </div>
-  )
+  );
 }
 
-export default TodoAppim
+export default TodoAppim;
