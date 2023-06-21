72a34c249a60c994eb135617a389406ac8f085ea
SametKiybik <samettemas05@gmail.com>
2023-06-21 07:20:00 +0300

    app

--- src/Component/TodoAppim.jsx
+++ src/Component/TodoAppim.jsx
@@ -1,30 +1,39 @@
-import React, { useState } from 'react'
+import React, { useEffect,useState } from 'react'
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
 
 function TodoAppim() {
+  
+  const [inputVisible, setInputVisible] = useState(false)
+  
 
-  const [inputVisible, setInputVisible] =  useState(false)
 
-
-  const resimTikla =()=>{
+  const resimTikla = () => {
     setInputVisible(!inputVisible)
   }
+
+ 
+
   return (
     <div className='note'>To-Do List
       <div className='noteContent'>
-        <InputGroup className="my-3 px-5 " style={{  visibility: inputVisible ? 'visible' : 'hidden'}}>
+        <InputGroup className="my-3 px-5 " style={{ visibility: inputVisible ? 'visible' : 'hidden' }} onKeyUp={}>
           <Form.Control
             placeholder="Please add a task"
             aria-label="Recipient's username"
             aria-describedby="basic-addon2"
           />
-          <Button variant="outline-secondary" id="button-addon2">
+          <Button variant="outline-secondary" id="button-addon2" onClick={}>
             Add
           </Button>
+          {
+             
+          }
+
         </InputGroup>
-        <img src='images/notepad.png' className='noteImg' onClick={resimTikla}/>
+        <img src='images/notepad.png' className='noteImg' onClick={resimTikla} />
+
 
       </div>
 
